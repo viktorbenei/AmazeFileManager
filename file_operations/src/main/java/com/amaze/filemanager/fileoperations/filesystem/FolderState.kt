86496ba60b8db26e1ff54353579dac75b68edc93
Viktor Benei <viktorbenei@bitrise.io>
2023-09-02 19:18:41 +0200

    One more empty line change

--- file_operations/src/main/java/com/amaze/filemanager/fileoperations/filesystem/FolderState.kt
+++ file_operations/src/main/java/com/amaze/filemanager/fileoperations/filesystem/FolderState.kt
@@ -22,10 +22,9 @@ package com.amaze.filemanager.fileoperations.filesystem
 
 import androidx.annotation.IntDef
 
-
-
 const val DOESNT_EXIST = 0
 const val WRITABLE_OR_ON_SDCARD = 1
+
 
 // For Android 5
 const val CAN_CREATE_FILES = 2
